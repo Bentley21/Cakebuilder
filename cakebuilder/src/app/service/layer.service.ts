ead6b9852ebc3b61de74e6d1903a4426fda6b4b1
Darryl Bentley <darrylbentley582@gmail.com>
2025-07-09 19:06:19 -0400

    Cake

--- cakebuilder/src/app/service/layer.service.ts
+++ cakebuilder/src/app/service/layer.service.ts
@@ -1,4 +1,13 @@
 import {Injectable} from '@angular/core';
 import {layer} from '../model/layer';
 
-Injectable()
+Injectable({
+    providedIn: 'root'
+})
+export class LayerService {
+
+    constructor() { }
+    getLayer() : layer[]
+        return this.layer;
+
+}
