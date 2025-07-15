2b00fa35871399bb50e9b4d2a4744c6b38234d16
Darryl Bentley <darrylbentley582@gmail.com>
2025-07-15 16:28:14 -0400

    Cake

--- cakebuilder/src/app/app.ts
+++ cakebuilder/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CakeBuilder } from "./components/cake-builder/cake-builder";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CakeBuilder],
+  imports: [RouterOutlet, CakeBuilder , RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/model/layer.ts
+++
@@ -1,9 +0,0 @@
-export interface layer {
-id: number
-height : number
-width  : number
-color : string 
-
-
-
-}
